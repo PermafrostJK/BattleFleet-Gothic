16fe18cfea4f6aebe7b70568757c858e50ec1bad
Ella_Zhang <74286996+ellazhang-gif@users.noreply.github.com>
2020-12-06 14:07:54 +0800

    Update game.py

--- game.py
+++ game.py
@@ -141,7 +141,7 @@ def __main__():
     #如果是后者，解决方法可能是要用在写所有东西之前按lable分类，建立两个class，再分别对他们进行操作#
     #实际上lable是更高一级的分类，四艘船都应该属于这个lable#
 
-    while i in range(10): #由于时间限制模式要和chat system结合，目前只写限定攻击次数的模式，这里面先用10来计算，其实也可以让用户自选回合数#
+    for i in range(10): #由于时间限制模式要和chat system结合，目前只写限定攻击次数的模式，这里面先用10来计算，其实也可以让用户自选回合数#
         x = input("Guess one x coordinate of your opponent's ship ")
         y = input("Guess one y coordinate of your opponent's ship ")
         Battlefield.update_battlefield(x,y)
