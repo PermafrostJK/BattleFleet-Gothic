9dd5fc19a4565e0174470bb1815e2d5d622f0cd4
Jialin Wang <65531347+JialinWangClaire@users.noreply.github.com>
2020-12-10 10:33:22 +0800

    Update chat_utils.py

--- chat_utils.py
+++ chat_utils.py
@@ -16,13 +16,13 @@ menu = "\n++++ Choose one of the following commands\n \
         ? _term_: to search your chat logs where _term_ appears\n \
         p _#_: to get number <#> sonnet\n \
         q: to leave the chat system\n\n \
-        g: to start the Ship Battle Game\n\n"
+        game: to start the Ship Battle Game\n\n"
 
 S_OFFLINE   = 0
 S_CONNECTED = 1
 S_LOGGEDIN  = 2
 S_CHATTING  = 3
-S_GAMING=4
+S_GAMING = 4
 
 SIZE_SPEC = 5
 
